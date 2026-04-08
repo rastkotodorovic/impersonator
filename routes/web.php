e4d9118aacdc6f2c37d0d01c03e1dd9646738b8d
Rastko <rale.todorovic2@gmail.com>
2026-04-08 23:12:46 +0200

    Remove root page

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,6 @@ use App\Http\Controllers\WhatsappController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
